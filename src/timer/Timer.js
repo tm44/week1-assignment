a1b395459c3540dc7ce688a75e0ea77cc9a255e6
Mike Tate <shagee@uw.edu>
2021-04-12 20:17:11 -0700

    Homework week 1 - remove commented lines

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,19 +1,3 @@
-// function Timer(seconds) {
-//   this.seconds = seconds;
-// }
-
-// Timer.prototype.start = function () {
-//   var instance = this;
-//   var timerInterval = setInterval(function () {
-//     if (instance.seconds === 0) {
-//       clearInterval(timerInterval);
-//     }
-
-//     console.log(instance.seconds);
-//     instance.seconds -= 1;
-//   }, 1000);
-// };
-
 class Timer {
   constructor(seconds) {
     this.seconds = seconds;
